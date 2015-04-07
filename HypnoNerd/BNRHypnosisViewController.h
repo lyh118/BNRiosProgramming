f0b4afb3fd234a0cce49a459a1c7d5e082212fd0
N3962 <n3962@N3962ui-Mac-mini.local>
2015-04-07 16:10:54 +0900

    CHAP6.HypnoNerd Start

---
+++ HypnoNerd/BNRHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.h
+//  HypnoNerd
+//
+//  Created by N3962 on 2015. 4. 7..
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRHypnosisViewController : UIViewController
+
+@end
---
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.m
+//  HypnoNerd
+//
+//  Created by N3962 on 2015. 4. 7..
+//
+//
+
+#import "BNRHypnosisViewController.h"
+
+@implementation BNRHypnosisViewController
+
+@end
